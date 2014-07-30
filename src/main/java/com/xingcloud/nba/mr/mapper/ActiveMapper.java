ad300390c5ab686905521610e6370664330fa49f
dannyhxwang <dannyhxwang@gmail.com>
2014-07-30 16:25:03 +0800

    change

--- src/main/java/com/xingcloud/nba/mr/mapper/ActiveMapper.java
+++ src/main/java/com/xingcloud/nba/mr/mapper/ActiveMapper.java
@@ -5,8 +5,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileSplit;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import java.io.IOException;
 
