fcb1ddb771d6d682b5fb9fc527b6da68e358b0ca
dannyhxwang <dannyhxwang@gmail.com>
2014-08-04 14:23:21 +0800

    change

--- src/main/java/com/xingcloud/nba/mr/reducer/ActiveReducer.java
+++ src/main/java/com/xingcloud/nba/mr/reducer/ActiveReducer.java
@@ -1,26 +1,23 @@
 package com.xingcloud.nba.mr.reducer;
 
 import com.xingcloud.nba.mr.model.JoinData;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 
 /**
  * Created by wanghaixing on 14-7-29.
  */
-public class ActiveReducer extends Reducer<Text, JoinData, Text, NullWritable> {
+public class ActiveReducer extends Reducer<Text, Text, Text, NullWritable> {
 
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        context.getCounter("ActiveCounter", "uidCounts").increment(1L);
-//        context.write(key, NullWritable.get());
+        Counter counter = context.getCounter("ActiveCounter", "uidCounts");
+        counter.increment(1L);
+        context.write(key, NullWritable.get());
     }
 }
+
