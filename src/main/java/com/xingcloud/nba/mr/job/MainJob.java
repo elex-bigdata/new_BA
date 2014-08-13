2e6b23d446a7399d36f68a8f19d85aed4fb7c8ff
dannyhxwang <dannyhxwang@gmail.com>
2014-08-13 09:30:06 +0800

    change

--- src/main/java/com/xingcloud/nba/mr/job/MainJob.java
+++ src/main/java/com/xingcloud/nba/mr/job/MainJob.java
@@ -28,16 +28,16 @@ public class MainJob {
             List<String> specialList = new ArrayList<String>();
             specialList.add("internet-1");
             specialList.add("internet-2");
-//            specialList.add("internet");
+            specialList.add("internet");
             Map<String, List<String>> specialProjectList = getSpecialProjectList();
 
-            int ret1 = mainJob.runProjectJob(specialList, specialProjectList);
+            /*int ret1 = mainJob.runProjectJob(specialList, specialProjectList);
             if(ret1 == 0) {
                 mainJob.runAnalyzeJob(specialList, specialProjectList);
             }
             mainJob.runInternetJob(Constant.ACT_UNIQ);
             //所有的数据都生成完毕
-            LOG.info("the raw uids all generated to /user/hadoop/offline/uid/................");
+            LOG.info("the raw uids all generated to /user/hadoop/offline/uid/................");*/
 
 //------------------------------------------------------------------------------------------------------
 
@@ -78,15 +78,15 @@ public class MainJob {
 
 //            new StoreResult("internet-1").storeNewUserNum(460168L);
 
-            long[][] activeCounts = new long[3][3];
+            /*long[][] activeCounts = new long[3][3];
             specialList.add("internet");
             for(int i = 0; i < 3; i++) {
                 mainJob.runActiveJob(specialList.get(i), activeCounts[i]);
                 //将统计好的活跃量放入redis中
                 new StoreResult(specialList.get(i)).storeActive(activeCounts[i]);
-            }
+            }*/
 
-            /*long[][] activeCounts = new long[3][3];
+            long[][] activeCounts = new long[3][3];
             activeCounts[0][0] = 12302236;
             activeCounts[0][1] = 28824674;
             activeCounts[0][2] = 49186157;
@@ -95,7 +95,12 @@ public class MainJob {
             activeCounts[1][2] = 44919828;
             activeCounts[2][0] = 24240176;
             activeCounts[2][1] = 45488011;
-            activeCounts[2][2] = 66708380;*/
+            activeCounts[2][2] = 66708380;
+            for(int i = 0; i < 3; i++) {
+                mainJob.runActiveJob(specialList.get(i), activeCounts[i]);
+                //将统计好的活跃量放入redis中
+                new StoreResult(specialList.get(i)).storeActive(activeCounts[i]);
+            }
 
             /*ActiveJob r = new ActiveJob("internet-1", 3);
             Thread t = new Thread(r);
