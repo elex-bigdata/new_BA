b1de7708e08b79d78168ab9c608538e4925a0158
griffinl <lq0987@126.com>
2014-08-28 14:31:14 +0800

    init

--- src/main/java/com/xingcloud/nba/task/BASQLGenerator.java
+++ src/main/java/com/xingcloud/nba/task/BASQLGenerator.java
@@ -40,7 +40,7 @@ public class BASQLGenerator {
     public static String getTransRegisterTimeUIDSql(String project, List<String> pids){
         StringBuffer sb = new StringBuffer();
         sb.append("insert overwrite table user_register_time  partition(pid='").append(project).append("')")
-                .append("select ui.orig_id, substr(max(up.val),0,8), substr(min(up.val),0,8) from user_property up join user_id ui on up.uid = ui.uid and up.pid = ui.pid ")
+                .append("select ui.orig_id, substr(max(up.val),0,8), substr(min(up.val),0,8) from user_property up join user_id ui on up.uid = md5uid(ui.uid) and up.pid = ui.pid ")
                 .append("and  up.prop = 'register_time' and up.pid in ('").append(pids.get(0)).append("'");
 
         for(int i=1;i<pids.size();i++){
@@ -56,11 +56,19 @@ public class BASQLGenerator {
         return sb.toString();
     }
 
+    public static String getCombineRegisterTimeUIDSql(String project, String[] projects){
+        StringBuffer sb = new StringBuffer();
+        sb.append("insert overwrite table user_register_time  partition(pid='").append(project).append("') ")
+                .append("select orig_id,max(max_reg_time), min(min_reg_time) from user_register_time where pid in ('")
+                .append(projects[0]).append("','").append(projects[1]).append("') group by orig_id");
+        return sb.toString();
+    }
+
     public static String getTransGeoIPUIDSql(String project, List<String> pids){
         StringBuffer sb = new StringBuffer();
 
         sb.append("insert overwrite table user_geoip  partition(pid='").append(project).append("')")
-                .append("select distinct ui.orig_id, up.val from user_property up join user_id ui on up.uid = ui.uid and up.pid = ui.pid ")
+                .append("select distinct ui.orig_id, up.val from user_property up join user_id ui on up.uid = md5uid(ui.uid) and up.pid = ui.pid ")
                 .append("and  up.prop = 'geoip' and  up.pid in ('").append(pids.get(0)).append("'");
         for(int i=1;i<pids.size();i++){
             sb.append(",'").append(pids.get(i)).append("'");
