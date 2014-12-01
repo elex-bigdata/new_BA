384ee31d4d95aa0b915cfe00d7e705b6039de614
griffinl <lq0987@126.com>
2014-12-01 17:04:33 +0800

    trans

---
+++ src/main/java/com/xingcloud/nba/model/CacheModel.java
@@ -0,0 +1,58 @@
+package com.xingcloud.nba.model;
+
+import java.math.BigDecimal;
+
+/**
+ * Author: liqiang
+ * Date: 14-12-1
+ * Time: 下午3:47
+ */
+public class CacheModel {
+
+    private int userNum = 0;
+    private int userTime = 0;
+    private BigDecimal value = new BigDecimal(0);
+
+    public int getUserNum() {
+        return userNum;
+    }
+
+    public void setUserNum(int userNum) {
+        this.userNum = userNum;
+    }
+
+    public int getUserTime() {
+        return userTime;
+    }
+
+    public void setUserTime(int userTime) {
+        this.userTime = userTime;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public void incrTime(){
+        userTime++;
+    }
+
+    public void incrValue(BigDecimal v){
+        value = value.add(v);
+    }
+
+    public void incrSameUser(BigDecimal v){
+        userTime++;
+        value = value.add(v);
+    }
+
+    public void incrDiffUser(CacheModel cm){
+        this.userTime ++;
+        this.userNum ++;
+        incrValue(cm.getValue());
+    }
+}
